e5b80c554a0879766fe02657a799957ee95a0b6c
Jetsons <info@github.com>
2019-04-19 09:23:51 +0530

    char utils

---
+++ Types/Chars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jetsons.JetPack {
+
+	/// <summary>
+	/// Commonly used character codes
+	/// </summary>
+	public static class Chars {
+
+		public const string Tab = "\t";
+
+		public const string NL = "\n";
+		public const string WinNL = "\r\n";
+		public const string NL2 = "\n\n";
+		public const string WinNL2 = "\r\n\r\n";
+
+	}
+}
