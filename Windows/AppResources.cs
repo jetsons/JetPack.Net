754fd0bdf2e6f9fee2cf68363a1824946c86e78b
Jetsons <info@github.com>
2019-04-19 09:24:52 +0530

    winforms app resource utils

---
+++ Windows/AppResources.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace Jetsons.JetPack {
+	public static class AppResources {
+		
+		/// <summary>
+		/// Extracts the resource at the given namespace path and returns it as a string
+		/// </summary>
+		public static string GetText(string resourcePath) {
+			var assembly = Assembly.GetEntryAssembly();
+			using (Stream stream = assembly.GetManifestResourceStream(resourcePath)) {
+				using (StreamReader reader = new StreamReader(stream)) {
+					string result = reader.ReadToEnd();
+					return result;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Extracts the resource at the given namespace path and returns it as an Icon
+		/// </summary>
+		public static Icon GetIcon(string resourcePath) {
+			var assembly = Assembly.GetEntryAssembly();
+			using (Stream stream = assembly.GetManifestResourceStream(resourcePath)) {
+				return new Icon(stream);
+			}
+		}
+
+		/// <summary>
+		/// Extracts the resource at the given namespace path and returns it as a Bitmap
+		/// </summary>
+		public static Bitmap GetBitmap(string resourcePath) {
+			var assembly = Assembly.GetEntryAssembly();
+			using (Stream stream = assembly.GetManifestResourceStream(resourcePath)) {
+				return new Bitmap(stream);
+			}
+		}
+
+		/// <summary>
+		/// Extracts the resource at the given namespace path and returns it as a byte array
+		/// </summary>
+		public static byte[] GetBytes(string resourcePath) {
+			var assembly = Assembly.GetEntryAssembly();
+			using (Stream stream = assembly.GetManifestResourceStream(resourcePath)) {
+				return stream.ToBytes();
+			}
+		}
+
+	}
+}
