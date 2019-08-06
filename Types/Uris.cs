b5673582086bc3a6e94afd10a178d5cef392c154
Jetsons <info@github.com>
2019-08-06 23:12:31 +0530

    new download URL to file

---
+++ Types/Uris.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jetsons.JetPack {
+	public static class Uris {
+
+		/// <summary>
+		/// Downloads the given URL to the given filepath.
+		/// If async is true, the file is downloaded in the background and control returns to the thread.
+		/// Returns true if the file was (re)downloaded.
+		/// </summary>
+		public static bool DownloadURLToFile(this string url, string filePath, bool overwrite = true, bool async = false) {
+			if (!overwrite && filePath.FileExists()) {
+				return false;
+			}
+			using (var client = new WebClient()) {
+				if (async) {
+					client.DownloadFileAsync(new Uri(url), filePath);
+				}
+				else {
+					client.DownloadFile(url, filePath);
+				}
+				return true;
+			}
+		}
+	}
+}
