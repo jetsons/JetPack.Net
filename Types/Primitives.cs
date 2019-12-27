92ef95945ecdb74d5b07d183985db7a3f7ae4910
Jetsons <info@github.com>
2019-12-27 23:00:57 +0530

    speeden up primitive conversion

--- Types/Primitives.cs
+++ Types/Primitives.cs
@@ -30,38 +30,170 @@ namespace Jetsons.JetPack {
 		/// <summary>
 		/// Converts or casts the given value to an integer
 		/// </summary>
-		public static int ToInt(this object value) {
+		public static int ToInt(this object value, int defaultValue = 0) {
 			if (value is int) {
 				return (int)value;
 			}
-			return Convert.ToInt32(value);
+			if (value is uint) {
+				return (int)((uint)value);
+			}
+			if (value is float) {
+				return (int)((float)value);
+			}
+			if (value is double) {
+				return (int)((double)value);
+			}
+			if (value is byte) {
+				return (int)((byte)value);
+			}
+			if (value is sbyte) {
+				return (int)((sbyte)value);
+			}
+			if (value is long) {
+				return (int)((long)value);
+			}
+			if (value is ulong) {
+				return (int)((ulong)value);
+			}
+			if (value is decimal) {
+				return (int)((decimal)value);
+			}
+			if (value is bool) {
+				return (bool)value == true ? 1 : 0;
+			}
+			if (value is string) {
+				int result;
+				if (int.TryParse((string)value, out result)) {
+					return result;
+				}
+			}
+			return defaultValue;
 		}
 		/// <summary>
 		/// Converts or casts the given value to an unsigned int
 		/// </summary>
-		public static uint ToUInt(this object value) {
+		public static uint ToUInt(this object value, uint defaultValue = 0u) {
 			if (value is uint) {
 				return (uint)value;
 			}
-			return Convert.ToUInt32(value);
+			if (value is int) {
+				return (uint)((int)value);
+			}
+			if (value is float) {
+				return (uint)((float)value);
+			}
+			if (value is double) {
+				return (uint)((double)value);
+			}
+			if (value is byte) {
+				return (uint)((byte)value);
+			}
+			if (value is sbyte) {
+				return (uint)((sbyte)value);
+			}
+			if (value is long) {
+				return (uint)((long)value);
+			}
+			if (value is ulong) {
+				return (uint)((ulong)value);
+			}
+			if (value is decimal) {
+				return (uint)((decimal)value);
+			}
+			if (value is bool) {
+				return (bool)value == true ? 1u : 0u;
+			}
+			if (value is string) {
+				uint result;
+				if (uint.TryParse((string)value, out result)) {
+					return result;
+				}
+			}
+			return defaultValue;
 		}
 		/// <summary>
 		/// Converts or casts the given value to a float
 		/// </summary>
-		public static float ToFloat(this object value) {
+		public static float ToFloat(this object value, float defaultValue = 0f) {
 			if (value is float) {
 				return (float)value;
 			}
-			return Convert.ToSingle(value);
+			if (value is double) {
+				return (float)((double)value);
+			}
+			if (value is decimal) {
+				return (float)((decimal)value);
+			}
+			if (value is int) {
+				return (float)((int)value);
+			}
+			if (value is uint) {
+				return (float)((uint)value);
+			}
+			if (value is byte) {
+				return (float)((byte)value);
+			}
+			if (value is sbyte) {
+				return (float)((sbyte)value);
+			}
+			if (value is long) {
+				return (float)((long)value);
+			}
+			if (value is ulong) {
+				return (float)((ulong)value);
+			}
+			if (value is bool) {
+				return (bool)value == true ? 1f : 0f;
+			}
+			if (value is string) {
+				float result;
+				if (float.TryParse((string)value, out result)) {
+					return result;
+				}
+			}
+			return defaultValue;
 		}
 		/// <summary>
 		/// Converts or casts the given value to a double
 		/// </summary>
-		public static double ToDouble(this object value) {
+		public static double ToDouble(this object value, double defaultValue = 0.0) {
 			if (value is double) {
 				return (double)value;
 			}
-			return Convert.ToDouble(value);
+			if (value is float) {
+				return (double)((float)value);
+			}
+			if (value is decimal) {
+				return (double)((decimal)value);
+			}
+			if (value is int) {
+				return (double)((int)value);
+			}
+			if (value is uint) {
+				return (double)((uint)value);
+			}
+			if (value is byte) {
+				return (double)((byte)value);
+			}
+			if (value is sbyte) {
+				return (double)((sbyte)value);
+			}
+			if (value is long) {
+				return (double)((long)value);
+			}
+			if (value is ulong) {
+				return (double)((ulong)value);
+			}
+			if (value is bool) {
+				return (bool)value == true ? 1.0 : 0.0;
+			}
+			if (value is string) {
+				double result;
+				if (double.TryParse((string)value, out result)) {
+					return result;
+				}
+			}
+			return defaultValue;
 		}
 		/// <summary>
 		/// Converts or casts the given value to a string
@@ -78,6 +210,10 @@ namespace Jetsons.JetPack {
 		public static bool ToBool(this object value) {
 			if (value is bool) {
 				return (bool)value;
+			}
+			if (value is string) {
+				var text = (string)value;
+				return text.EqualsCI("true") || text.EqualsCI("yes");
 			}
 			return Convert.ToBoolean(value);
 		}
